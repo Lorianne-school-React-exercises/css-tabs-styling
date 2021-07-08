f9f1c08669a7306c962766622150dd48cbd2116b
Lorianne Aguilar <78496780+Grailsidhe@users.noreply.github.com>
2021-07-08 11:00:45 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./styles.css"
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="tabs-container">
+        <div className="div div1">
+          <h2 className="tab-text">News</h2>
+        </div>
+        <div className="div div2">
+          <h2 className="tab-text">Weather</h2>
+        </div>
+        <div className="div div3">
+          <h2 className="tab-text">Currency</h2>
+        </div>
+        <div className="div div4">
+          <h2 className="tab-text">Health Watch</h2>
+        </div>
+      </div>
+    </div>
+  )
+}
